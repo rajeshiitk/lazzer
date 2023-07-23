a34641223004e6dced1308ad212bf02675de1b5b
Rajesh Choudhary <rajuvert18@gmail.com>
2023-07-23 12:53:39 +0530

    link at logout

--- src/app/profile/Navbar..tsx
+++ src/app/profile/Navbar..tsx
@@ -86,7 +86,9 @@ export  default function Navbar() {
               type="button"
               className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
             >
+              <Link href="/login">
               Logout
+              </Link>
             </button>
           </div>
           <div className="lg:hidden">
