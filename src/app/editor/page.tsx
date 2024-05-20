44dc04b0df0cec4bde0c816f7bf23d64c444efb0
Rajesh Choudhary <rajuvert18@gmail.com>
2024-05-20 23:43:50 +0530

    excalidraw integration

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -6,6 +6,13 @@ const Editor = dynamic(() => import("@/components/editor/Editor"), {
   ssr: false,
 });
 
+const ExcalidrawWrapper = dynamic(
+  async () => await import("@/components/ExcalidrawWrapper"),
+  {
+    ssr: false,
+  }
+);
+
 const INITIAL_DATA = {
   time: new Date().getTime(),
   blocks: [
@@ -22,38 +29,42 @@ const Page = () => {
   const [data, setData] = useState(INITIAL_DATA);
   const [resetTrigger, setResetTrigger] = useState(false);
   return (
-    <div className="h-[calc(100svh)] w-full overflow-auto sm:w-1/2 flex flex-col justify-end p-4">
-      <div className="editor h-[calc(100svh_-_56px)] relative gap-2 py-6 sm:px-6  justify-end w-full">
-        <div className="flex gap-8 z-10 top-0 left-0 justify-evenly">
-          <Button
-            variant="secondary"
-            className="px-4 savebtn w-full"
-            onClick={() => {
-              alert(JSON.stringify(data));
-            }}
-          >
-            Save
-          </Button>
-          <Button
-            variant="destructive"
-            className="px-4 savebtn w-full"
-            onClick={() => {
-              const newData = { ...INITIAL_DATA, time: new Date().getTime() };
-              setData(newData);
-              setResetTrigger(!resetTrigger);
-              // alert(JSON.stringify(newData));
-            }}
-          >
-            Reset
-          </Button>
-        </div>
+    <div className="flex flex-col sm:flex-row">
+      <div className="h-[calc(100svh)]  w-full overflow-auto sm:w-1/3 flex flex-col justify-end p-4">
+        <div className="editor h-[calc(100svh_-_56px)] relative gap-2 py-6 sm:px-6  justify-end w-full">
+          <div className="flex gap-8 z-10 top-0 left-0 justify-evenly">
+            <Button
+              variant="secondary"
+              className="px-4 savebtn w-full"
+              onClick={() => {
+                alert(JSON.stringify(data));
+              }}
+            >
+              Save
+            </Button>
+            <Button
+              variant="destructive"
+              className="px-4 savebtn w-full"
+              onClick={() => {
+                const newData = { ...INITIAL_DATA, time: new Date().getTime() };
+                setData(newData);
+                setResetTrigger(!resetTrigger);
+              }}
+            >
+              Reset
+            </Button>
+          </div>
 
-        <Editor
-          data={data}
-          onChange={setData}
-          editorblock="editorjs-container"
-          resetTrigger={resetTrigger}
-        />
+          <Editor
+            data={data}
+            onChange={setData}
+            editorblock="editorjs-container"
+            resetTrigger={resetTrigger}
+          />
+        </div>
+      </div>
+      <div className=" flex flex-col justify-end  sm:w-2/3 ">
+        <ExcalidrawWrapper />
       </div>
     </div>
   );
---
+++ src/components/ExcalidrawWrapper.tsx
@@ -0,0 +1,23 @@
+"use client";
+import {
+  Excalidraw,
+  convertToExcalidrawElements,
+} from "@excalidraw/excalidraw";
+// import type { ExcalidrawElement } from "@excalidraw/excalidraw/dist/excalidraw/element/types";
+
+// import "@excalidraw/excalidraw/index.css";
+
+const ExcalidrawWrapper: React.FC = () => {
+  // console.info(convertToExcalidrawElements([{
+  //   type: "rectangle",
+  //   id: "rect-1",
+  //   width: 186.47265625,
+  //   height: 141.9765625,
+  // },]));
+  return (
+    <div className="w-full h-[calc(100svh)] sm:h-[calc(100svh_-_56px)]">
+      <Excalidraw />
+    </div>
+  );
+};
+export default ExcalidrawWrapper;
